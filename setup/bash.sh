65e7880632d28b0f82f8b5f5d69d0a341e841aff
airseneo <temfack2arsene@gmail.com>
2021-12-20 12:50:50 -0600

    bashrc

--- setup/bash.sh
+++ setup/bash.sh
@@ -10,11 +10,12 @@ CONFIG_S=$(pwd)/conf.d
 CONFIG_D=$HOME
 
 MSG="Setup Bash personalized configuration files:\n
-\t$CONFIG_D/.bashrc\n
 \t$CONFIG_D/.bash_fancy\n
 \t$CONFIG_D/.bash_aliases\n
-Copied from $CONFIG_S\n\n
-\tUsage: $0 [ --help | -h | help ]\n"
+ Copied from \`$CONFIG_S\`\n\n
+\tUsage: $0 [ --help | -h | help ]\n\n
+\n
+Note: This scropt will overwrite existing config files."
 
 if [[ "$#" = "1" ]] &&
 		[[ "$1" = "--help" || "$1" = "-h" || "$1" = "help" ]]; then
@@ -25,17 +26,38 @@ if [[ "$#" != "0" ]]; then
 	echo -e $MSG; exit 1
 fi
 
-
 if [ ! -d $CONFIG_S ]; then
 	echo "Missing directory: $CONFIG_S\n	Use opion --help for help"; exit 1
 fi
 
-if [[ ! -f $CONFIG_S/bashrc || ! -f $CONFIG_S/bash_aliases ||
-		! -f $CONFIG_S/bash_fancy ]]; then
+if [[ ! -f $CONFIG_S/bash_aliases || ! -f $CONFIG_S/bash_fancy ]]; then
 	echo "Missing configuration file(s)."; exit 1
 fi
 
-cp $CONFIG_S/bashrc $CONFIG_D/.bashrc
+BASHRC=$CONFIG_D/.bashrc
+
+BASHRC_FANCY="\n# Fancy bash\n
+if [ -f ~/.bash_fancy ]; then\n
+\t. ~/.bash_fancy\n
+fi"
+
+BASHRC_ALIASES="\n# Alias definitions\n
+if [ -f ~/.bash_aliases ]; then\n
+\t. ~/.bash_aliases\n
+fi"
+
+if [[ ! -f $BASHRC ]]; then
+	echo -e $BASHRC_FANCY > $BASHRC
+	echo -e $BASHRC_ALIASES >> $BASHRC
+else
+	if [[ `grep ". ~/.bash_fancy$" $BASHRC | wc -l` == "0" ]]; then
+		echo -e $BASHRC_FANCY >> $BASHRC
+	fi
+	if [[ `grep ". ~/.bash_aliases$" $BASHRC | wc -l` == "0" ]]; then
+		echo -e $BASHRC_FANCY >> $BASHRC
+	fi
+fi
+
 cp $CONFIG_S/bash_aliases $CONFIG_D/.bash_aliases
 cp $CONFIG_S/bash_fancy $CONFIG_D/.bash_fancy
 
